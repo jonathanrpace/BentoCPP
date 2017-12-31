f3da4499e63fe51126c824eb69820be46fbb6eab
Jonathan Pace <jonathanrpace@hotmail.com>
2017-12-31 12:47:26 +0000

    Lava material map has high-passed height channel

--- Resources/textures/Compress.bat
+++ Resources/textures/Compress.bat
@@ -1,14 +1,6 @@
-nvdxt -file Lava_Albedo.tga -outdir   ../../Run/textures/ -quality_highest -dxt5   -timestamp -sharpenMethod SharpenMedium
-nvdxt -file Lava_Material.tiff -outdir ../../Run/textures/ -quality_highest -dxt5 -timestamp -nmips 6
+nvdxt -file Lava_Albedo.tif -outdir   ../../Run/textures/ -quality_highest -dxt5 -timestamp -nmips 6
+nvdxt -file Lava_Material.tif -outdir ../../Run/textures/ -quality_highest -dxt5 -timestamp -nmips 6
 nvdxt -file Lava_Normal.tga -outdir   ../../Run/textures/ -quality_highest -dxt5nm -timestamp
-
-nvdxt -file LavaLat_Albedo.tga -outdir   ../../Run/textures/ -quality_highest -dxt5   -timestamp -sharpenMethod SharpenMedium
-nvdxt -file LavaLat_Material.tga -outdir ../../Run/textures/ -quality_highest -dxt5   -timestamp -sharpenMethod SharpenMedium
-nvdxt -file LavaLat_Normal.tga -outdir   ../../Run/textures/ -quality_highest -dxt5nm -timestamp
-
-nvdxt -file LavaLong_Albedo.tga -outdir   ../../Run/textures/ -quality_highest -dxt5   -timestamp -sharpenMethod SharpenMedium
-nvdxt -file LavaLong_Material.tga -outdir ../../Run/textures/ -quality_highest -dxt5   -timestamp -sharpenMethod SharpenMedium
-nvdxt -file LavaLong_Normal.tga -outdir   ../../Run/textures/ -quality_highest -dxt5nm -timestamp
 
 nvdxt -file SmokeParticle.tga -outdir   ../../Run/textures/ -quality_highest -dxt5 -timestamp
 
